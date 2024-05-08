6364de2e4395bbe31528a9f38896239c86ca738d
Farhad K <farhadk8552@gmail.com>
2024-05-08 11:06:29 +0100

    CustomerSupport Tabel Test

--- ClassLibrary/clsCustomerSupport.cs
+++ ClassLibrary/clsCustomerSupport.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomerSupport
     {
+        public bool Active { get; set; }
+        public int TicketID { get; set; }
+        public string TicketType { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public string TicketStatus { get; set; }
+        public bool RaiseTicketStatus { get; set; }
     }
 }
--- Testing2/tstCustomerSupport.cs
+++ Testing2/tstCustomerSupport.cs
@@ -15,5 +15,143 @@ namespace Testing2
             //test to see that it exists
             Assert.IsNotNull(AnCustomerSupport); 
         }
+
+        [TestMethod]
+
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+            Boolean TestData = true;
+            //assign the data to the property
+            AnCustomerSupport.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.Active, TestData);
+        }
+        [TestMethod]
+        public void TicketIDPropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+            Int32 TestData = 1;
+
+            //assign the data to the property
+            AnCustomerSupport.TicketID = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.TicketID, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void TicketTypePropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+            string TestData = "Techniqal support";
+
+            //assign the data to the property
+            AnCustomerSupport.TicketType = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.TicketType, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void SubjectPropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+            string TestData = "Error";
+
+            //assign the data to the property
+            AnCustomerSupport.Subject = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.Subject, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void DescriptionPropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+            string TestData = "Full Deatails";
+
+            //assign the data to the property
+            AnCustomerSupport.Description = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.Description, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void SubmissionDatePropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+           DateTime TestData = DateTime.Now.Date;
+
+            //assign the data to the property
+            AnCustomerSupport.SubmissionDate = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.SubmissionDate, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void TicketStatusPropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+            string TestData = "Open";
+
+            //assign the data to the property
+            AnCustomerSupport.TicketStatus = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.TicketStatus, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void RaiseTicketPropertyOK()
+        {
+            //Create an  instance of the class we want to create
+            clsCustomerSupport AnCustomerSupport = new clsCustomerSupport();
+
+            //create some test data to assing to the property
+            Boolean TestData = true;
+
+            //assign the data to the property
+            AnCustomerSupport.RaiseTicketStatus = TestData;
+
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomerSupport.RaiseTicketStatus, TestData);
+
+
+        }
     }
 }
