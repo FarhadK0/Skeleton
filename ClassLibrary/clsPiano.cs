b852e482f3fc95bdb4f4f690f7fdfd3be9580f9a
Wataru N. <p2708490@my365.dmu.ac.uk>
2024-05-11 12:10:58 +0100

    Created tests for a find method for all attributes; forced pass, hardcoded

--- ClassLibrary/clsPiano.cs
+++ ClassLibrary/clsPiano.cs
@@ -4,12 +4,120 @@ namespace ClassLibrary
 {
     public class clsPiano
     {
-        public int PianoId { get; set; }
-        public DateTime DateAdded { get; set; }
-        public double Price { get; set; }
-        public string Manufacturer { get; set; }
-        public string ModelName { get; set; }
-        public bool IsInStock { get; set; }
-        public string SerialNumber { get; set; }
+        // private data member for all attributes
+        private Int32 mPianoId;
+        private DateTime mDateAdded;
+        private double mPrice;
+        private String mManufacturer;
+        private String mModelName;
+        private bool mIsInStock;
+        private String mSerialNumber;
+
+        public int PianoId
+        {
+            get
+            {
+                // sends out the property
+                return mPianoId;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mPianoId = value;
+            }
+        }
+        public DateTime DateAdded
+        {
+            get
+            {
+                // sends out the property
+                return mDateAdded;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mDateAdded = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                // sends out the property
+                return mPrice;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mPrice = value;
+            }
+        }
+        public string Manufacturer
+        {
+            get
+            {
+                // sends out the property
+                return mManufacturer;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mManufacturer = value;
+            }
+        }
+        public string ModelName
+        {
+            get
+            {
+                // sends out the property
+                return mModelName;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mModelName = value;
+            }
+        }
+        public bool IsInStock
+        {
+            get
+            {
+                // sends out the property
+                return mIsInStock;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mIsInStock = value;
+            }
+        }
+        public string SerialNumber
+        {
+            get
+            {
+                // sends out the property
+                return mSerialNumber;
+            }
+            set
+            {
+                // retrieved data and sets it to property
+                mSerialNumber = value;
+            }
+        }
+
+        public bool Find(int pianoId)
+        {
+            //set private data members to test value
+            mPianoId = 1;
+            mDateAdded = Convert.ToDateTime("12/05/2024");
+            mPrice = 399.99;
+            mManufacturer = "Yamaha";
+            mModelName = "Upright B1";
+            mIsInStock = true;
+            mSerialNumber = "928719308";
+            
+            // Force always true
+            return true;
+        }
     }
 }
