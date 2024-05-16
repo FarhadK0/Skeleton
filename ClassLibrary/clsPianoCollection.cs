56adf7e9e98436c9690bd65461080dfcd09a73a4
Wataru N. <p2708490@my365.dmu.ac.uk>
2024-05-16 13:04:43 +0100

    Done until middle of page 12

---
+++ ClassLibrary/clsPianoCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsPianoCollection
+    {
+        //private data memebr for the list
+        List<clsPiano> mPianoList = new List<clsPiano>();
+
+        public clsPianoCollection()
+        {
+            //create an instance of a piano
+            clsPiano TestItem = new clsPiano();
+            //set its properties
+            TestItem.PianoId = 1;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.Price = 123.99;
+            TestItem.Manufacturer = "Yamaha";
+            TestItem.ModelName = "Electric Piano";
+            TestItem.IsInStock = true;
+            TestItem.SerialNumber = "jsdoasd0a9s";
+            //add the test item to the test list
+
+        }
+
+        public List<clsPiano> PianoList 
+        {
+            get
+            {
+                //return the private data
+                return mPianoList;
+            }
+            
+            set
+            { 
+                //set the private data
+                mPianoList = value; 
+            }
+        
+        }
+        public int Count 
+        {
+            get
+            {
+                //return the length of the list
+                return mPianoList.Count;
+            }
+            set
+            {
+                // TODO
+            }
+        }
+        public clsPiano ThisPiano { get; set; }
+    }
+}
---
+++ Testing3/tstPianoCollection.cs
@@ -0,0 +1,110 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstPianoCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create a new instance of allPianos
+            clsPianoCollection allPianos = new clsPianoCollection();
+            //test if it exists
+            Assert.IsNotNull(allPianos);
+        }
+
+        [TestMethod]
+        public void AddressListOK()
+        {
+            //create an instance of allPianos
+            clsPianoCollection allPianos = new clsPianoCollection();
+            //create some test data needs to be a list of objects
+            //list of objects
+            List<clsPiano> TestList = new List<clsPiano>();
+
+            //add a new item for the list
+            clsPiano TestItem = new clsPiano();
+            //set its properties
+            TestItem.PianoId = 21;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.Price = 499.99;
+            TestItem.Manufacturer = "Bosendorf";
+            TestItem.ModelName = "Grand Piano";
+            TestItem.IsInStock = true;
+            TestItem.SerialNumber = "s38-9fug-98";
+            //add the item to the list
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            allPianos.PianoList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(allPianos.PianoList, TestList);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instance of allPianos
+            clsPianoCollection allPianos = new clsPianoCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 0;
+            //assign the data to the property
+            allPianos.Count = SomeCount;
+            //test
+            Assert.AreEqual(allPianos.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisPianoPropertyOK()
+        {
+            //create an instance of allPianos
+            clsPianoCollection allPianos = new clsPianoCollection();
+            //create an instance of a piano
+            clsPiano TestItem = new clsPiano();
+            //set its properties
+            TestItem.PianoId = 21;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.Price = 499.99;
+            TestItem.Manufacturer = "Bosendorf";
+            TestItem.ModelName = "Grand Piano";
+            TestItem.IsInStock = true;
+            TestItem.SerialNumber = "s38-9fug-98";
+            //assign the data to the property
+            allPianos.ThisPiano = TestItem;
+            //test
+            Assert.AreEqual(allPianos.ThisPiano, TestItem);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of allPianos
+            clsPianoCollection allPianos = new clsPianoCollection();
+            //create some test data needs to be a list of objects
+            //list of objects
+            List<clsPiano> TestList = new List<clsPiano>();
+
+            //add a new item for the list
+            clsPiano TestItem = new clsPiano();
+            //set its properties
+            TestItem.PianoId = 21;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.Price = 499.99;
+            TestItem.Manufacturer = "Bosendorf";
+            TestItem.ModelName = "Grand Piano";
+            TestItem.IsInStock = true;
+            TestItem.SerialNumber = "s38-9fug-98";
+            //add the item to the list
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            allPianos.PianoList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(allPianos.Count, TestList.Count);
+        }
+    }
+}
