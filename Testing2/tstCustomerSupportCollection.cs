30bc0ad73ced5141ff1d0228beb8d1f11feb46b6
P2755047 <P2755047@GWHXX683WIN004W>
2024-05-17 15:51:49 +0100

    Practical 12 completed worked in presentation layer of customerSupportList

---
+++ Testing2/tstCustomerSupportCollection.cs
@@ -0,0 +1,122 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.AccessControl;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomerSupportCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsCustomerSupportCollection AllCustomerSupports = new clsCustomerSupportCollection ();
+
+            //test to see that is exists
+            Assert.IsNotNull (AllCustomerSupports);
+        }
+
+        [TestMethod]
+
+        public void CustomerSupportListOK()
+        {
+            //create an insatance of the class we want to create 
+            clsCustomerSupportCollection AllCutsomerSupports = new clsCustomerSupportCollection ();
+
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsCustomerSupport> TestList = new List<clsCustomerSupport> ();
+
+            //Add an Item of the list
+            //create the item of test data
+            clsCustomerSupport TestItem = new clsCustomerSupport();
+
+            //set its properties
+            TestItem.TicketID = 1;
+            TestItem.TicketType = "Technical Support";
+            TestItem.Subject = "reset password";
+            TestItem.Description = "cannot reset the password";
+            TestItem.SubmissionDate = DateTime.Now;
+            TestItem.TicketStatus = "InProgress";
+            TestItem.TicketElevated = true;
+
+            //add the item to the test list
+            TestList.Add (TestItem);
+
+            //assign the data to the property
+            AllCutsomerSupports.CustomerSupportList = TestList;
+
+            //test to see that two values are the same
+            Assert.AreEqual(AllCutsomerSupports.CustomerSupportList, TestList);
+        }
+
+       
+
+        [TestMethod]
+
+        public void ThisCustomerSupportPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsCustomerSupportCollection AllCustomerSupports = new clsCustomerSupportCollection();
+
+            //create some test data to assign to the property
+            clsCustomerSupport TestCustomerSupport = new clsCustomerSupport();
+
+            //set the properties of the test object
+            TestCustomerSupport.TicketID = 1;
+            TestCustomerSupport.TicketType = "Technical Support";
+            TestCustomerSupport.Subject = "reset password";
+            TestCustomerSupport.Description = "cannot reset the password";
+            TestCustomerSupport.SubmissionDate = DateTime.Now;
+            TestCustomerSupport.TicketStatus = "InProgress";
+            TestCustomerSupport.TicketElevated= true;
+
+            //assign the data to the property
+            AllCustomerSupports.ThisCustomerSupport = TestCustomerSupport;
+
+            //test to see that two values are the same
+            Assert.AreEqual(AllCustomerSupports.ThisCustomerSupport, TestCustomerSupport);
+        }
+
+        [TestMethod]
+
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerSupportCollection AllCustomerSupports = new clsCustomerSupportCollection();
+
+            //create some test data to assign to the property
+            //in this case the data needs to be list of objects
+            List<clsCustomerSupport> TestList = new List<clsCustomerSupport>();
+
+            //Add an Item to the List
+            //create the item of test data
+            clsCustomerSupport TestItem = new clsCustomerSupport();
+
+            //set its properties
+            TestItem.TicketID = 1;
+            TestItem.TicketType = "Technical Support";
+            TestItem.Subject = "reset password";
+            TestItem.Description = "cannot reset the password";
+            TestItem.SubmissionDate = DateTime.Now;
+            TestItem.TicketStatus = "InProgress";
+            TestItem.TicketElevated = true;
+
+            //add the item to the test List
+            TestList.Add(TestItem);
+
+            //assign the data to the property
+            AllCustomerSupports.CustomerSupportList = TestList;
+
+            //test to see that two values are the same
+            Assert.AreEqual(AllCustomerSupports.Count, TestList.Count);
+
+        }
+
+       
+    }
+}
