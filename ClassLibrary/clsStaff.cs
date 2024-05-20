18f3758e381e82a042f72f0de3332124797eb19b
P2730950 <P2730950@GWHXX283WIN010W>
2024-05-20 14:19:34 +0100

    Validation

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -161,10 +161,135 @@ namespace ClassLibrary
 
 
 
+        }
+
+        public string Valid(string staffName, string staffEmail, string dateOfBirth, string staffAddress, string staffAge)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            //create  a temporary variable to store the date value 
+            DateTime DateTemp;
+            //create  a temporary variable to store the age value 
+            Int32 AgeTemp;
+            //if the StaffName is blank
+            if (staffName.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Staff Name may not be blank : ";
+
+            }
+            //if the StaffName is greater than 50
+            if (staffName.Length > 50)
+            {
+                //record the error
+                Error = Error + "The Staff Name must be less than 50 characters: ";
+
+            }
+
+            //if the StaffEmail is blank
+            if (staffEmail.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Staff Email may not be blank : ";
+
+            }
+
+            //if the StaffEmail is greater than 50
+            if (staffEmail.Length > 50)
+            {
+                //record the error
+                Error = Error + "The Staff Email must be less than 50 characters: ";
+
+            }
+
+            //if the StaffAddress is blank
+            if (staffAddress.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Staff Address may not be blank : ";
+
+            }
+
+            //if the StaffAddress is greater than 50
+            if (staffAddress.Length > 50)
+            {
+                //record the error
+                Error = Error + "The Staff Address must be less than 50 characters: ";
+
+            }
+
+            //Define the Date value to 18 years 
+            AgeTemp = Convert.ToInt32(staffAge);
+
+            //if the StaffAddress is greater than 50
+            if (AgeTemp > 65)
+            {
+                //record the error
+                Error = Error + "The Staff Age is too old: ";
+
+            }
+
+
+            //if the StaffAddress is greater than 50
+            if (AgeTemp < 18)
+            {
+                //record the error
+                Error = Error + "The Staff is too young: ";
+
+            }
 
 
 
 
+            // Define the extreme minimum date (100 years ago), todays date and min (-1 day)
+                DateTime ExtremeMinDate = DateTime.Today.AddYears(-100);
+                DateTime DateComp = new DateTime(2004, 1, 10);
+            
+               
+
+            try
+            {
+                // Copy the dateOfBirth value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(dateOfBirth);
+                // Check if the date of birth is less than the extreme minimum date
+                if (DateTemp < ExtremeMinDate)
+                {
+                    // Record the error
+                    Error = Error + "The date of birth is too far in the past: ";
+                }
+
+                // Check if the date of birth is greater than today's date
+                if (DateTemp > DateComp)
+                {
+                    // Record the error
+                    Error = Error + "The date of birth cannot be in the future: ";
+                }
+
+                // Check if the date of birth is greater than today's date
+                if (DateTemp == DateComp.AddDays(1))
+                {
+                    // Record the error
+                    Error = Error + "The date of birth cannot be in the future: ";
+                }
+
+                // Check if the date of birth is greater than today's date
+                if (DateTemp == DateComp.AddDays(-1))
+                {
+                    // Record the error
+                    Error = Error + "The date of birth is too far in the past: ";
+                }
+
+            }
+            catch (FormatException)
+            {
+              
+                // Record the error for invalid date format
+                Error = Error + "The date of birth is not a valid date: ";
+            }
+
+
+            //return any error messages
+            return Error;
         }
     }
 }
