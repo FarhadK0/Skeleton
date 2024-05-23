bc8721b2bd42f4a74631d6cded80f973b7d9ec12
P2755047 <P2755047@GWHXX683WIN004W>
2024-05-23 16:58:19 +0100

    Complete the login form for customer support

---
+++ AdminSystem/CustomerSupportLogin.aspx.cs
@@ -0,0 +1,66 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CustomerSupportLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        //create an instance of the CustomerSupport User Class
+        clsCustomerSupportUser AnUser = new clsCustomerSupportUser();
+
+        //create the variabels tp store the username and password
+        string UserName = txtUserName.Text;
+        string Password = txtPassword.Text;
+
+        //create a varibale to store the result of the user operation
+        Boolean Found = false;
+
+        //get the username entered by the user
+        UserName = Convert.ToString(txtUserName.Text);
+
+        //get the password entered by the user
+        Password = Convert.ToString(txtPassword.Text);
+
+        //find the record 
+        Found = AnUser.FindUser(UserName, Password);
+
+        //if username and/or password is empty
+        if (txtUserName.Text == "")
+        {
+            //record the error
+            lblError.Text = "Enter the Username";
+
+        }
+
+        
+        else if (txtPassword.Text == "")
+        {
+            //record the error
+            lblError.Text = "Enter the Password";
+        }
+
+        //if record found
+        else if (Found == true)
+            {
+            //redirect to the list Page
+            Response.Redirect("CustomerSupportList.aspx");
+        }
+
+        else if (Found ==  false) 
+        {
+            //record the error
+            lblError.Text = "Login Details are Incorrect. Please try again";
+        }
+                
+    }
+}
---
+++ ClassLibrary/clsCustomerSupportUser.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary
+{
+    public class clsCustomerSupportUser
+    {
+
+        //private data member for the UserID property
+        private Int32 cUserID;
+
+        //private data member for the UserName property
+        private String cUserName;
+
+        //private data member for the Password property
+        private string cPassword;
+
+        //private data member for the Department property
+        private string cDepartment;
+
+        public int UserID 
+        {
+            get
+            {
+                //return the private
+                return cUserID;
+            }
+            
+            set
+            {
+                //set the private data
+                cUserID = value;
+            }
+        }
+        public string UserName 
+        { 
+            get
+            {
+                //return the private
+                return cUserName;
+            }
+                set
+            {
+                //set the private data
+                cUserName = value;
+            }
+        }
+        public string Password 
+        {
+            get
+            {
+                //return the private
+                return cPassword;
+            }
+
+            set
+            {
+                //set the private data
+                cPassword = value;
+            }
+        }
+        public string Department 
+        {
+            get
+            {
+                //return the private
+                return cDepartment;
+            }
+
+            set
+            {
+                //set the private data
+                cDepartment = value;
+            }
+        }
+
+        public bool FindUser(string UserName, string Password)
+        {
+          //create an instance of the data connection
+          clsDataConnection DB = new clsDataConnection();
+
+            //add the parameters for the user user name and password to search for 
+            DB.AddParameter("@UserName", UserName);
+            DB.AddParameter("@Password", Password);
+
+            //execute the stored procedure
+            DB.Execute("sproc_tblUsers_FindUserNamePW");
+
+            //if one record is found (there should be eithe one or more)
+            if (DB.Count ==1)
+            {
+                //copy the data form the database to the private data members
+                cUserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                cUserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                cPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                cDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+              
+               //return true to confirm everthing worked ok
+               return true;
+               
+            }
+            else
+            {
+             return false;
+            }
+            
+
+        }
+    }
+}
